7aa40e034f066f1cbfb884a61e1a600e61c0e36a
Abheyjeet0007 <abheyjeet0007.becse24@chitkara.edu.in>
2025-02-01 20:45:01 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -10,7 +10,9 @@ int main() {
     if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
         printf("Vowel");
     }
-
+    else if(c>='0'&&c<='9'){
+        printf("Digit");
+    }
     else if(c>='A'&&c<='Z'||c>='a'&&c<='z'){
         printf("Consonant");
     }
