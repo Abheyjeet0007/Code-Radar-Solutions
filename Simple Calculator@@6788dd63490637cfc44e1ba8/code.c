faca69fd5a16eb8366d0ff0636655fff3f1f1638
Abheyjeet0007 <abheyjeet0007.becse24@chitkara.edu.in>
2025-02-14 18:47:30 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,16 @@
+// Your code here...
+int n,m;
+char c;
+scanf("%d%d%c",&n,&m,&c);
+if(c=='*'){
+    printf("%d%d",n*m);
+}
+else if(c == '/'){
+    printf("%d%d",n/m);
+}
+else if(c == '+'){
+    printf("%d%d",n+m);
+}
+else if(c == '-'){
+    printf("%d%d",n-m);
+}
