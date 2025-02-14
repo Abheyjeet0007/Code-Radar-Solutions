3a7e6ad8314670bf02ec3cb46ca8e0f9a5571a78
Abheyjeet0007 <abheyjeet0007.becse24@chitkara.edu.in>
2025-02-14 18:51:38 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,16 +1,20 @@
 // Your code here...
 int n,m;
 char c;
+float a = n/m;
+int b = n*m;
+int c = n+m;
+int d = n-m;
 scanf("%d %d %c",&n,&m,&c);
 if(c=='*'){
-    printf("%d%d",n*m);
+    printf("%d",b);
 }
 else if(c == '/'){
-    printf("%d%d",n/m);
+    printf("%d",a);
 }
 else if(c == '+'){
-    printf("%d%d",n+m);
+    printf("%d",c);
 }
 else if(c == '-'){
-    printf("%d%d",n-m);
+    printf("%d",d);
 }
