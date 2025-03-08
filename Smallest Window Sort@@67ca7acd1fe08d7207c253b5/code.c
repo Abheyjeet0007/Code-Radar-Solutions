1fcc3e41f55345a079c8014fd6466bd7efbd013e
Abheyjeet0007 <abheyjeet0007.becse24@chitkara.edu.in>
2025-03-08 23:12:38 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -2,11 +2,12 @@
 int main(){
     int t;
     scanf("%d",&n);
-    while(t--)
+    while(t--){
     int n;
     scanf("%d",&n);
     int arr[10000];
     for(int i = 1;i<=n;i++){
         printf("%d ",i);
+    }
     }
 }
