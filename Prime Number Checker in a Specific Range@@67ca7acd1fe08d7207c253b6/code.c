bfd5ea45e697a8711ff00d58aff13a287fdfe976
Abheyjeet0007 <abheyjeet0007.becse24@chitkara.edu.in>
2025-03-12 04:07:20 +0530

    Solution to Prime Number Checker in a Specific Range

--- Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
+++ Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
@@ -3,8 +3,8 @@ int printPrimesInRange(int n,int m){
     if(n<=1){
         return 0;
     }
-    for(int i = 2;i<=m;i++){
-            if(m%i==0){
+    for(int i = 2;i<=n;i++){
+            if(n%i==0){
                 printf("%d",n);
             }
             
